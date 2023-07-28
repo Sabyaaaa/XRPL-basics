35efc739e4b17b5077ad182319c6889be2c41fcf
Sabya <sabya7011@gmail.com>
2023-07-28 13:35:48 +0530

    create account and send xrp code

---
+++ account/get_acc_info.js
@@ -0,0 +1,25 @@
+/** This code prints the account info for a given (pre-defined) account */
+'use strict';
+console.log("connecting");
+const RippleAPI = require('ripple-lib').RippleAPI;
+
+const api = new RippleAPI({
+  server: 'wss://s1.ripple.com' // Public rippled server
+});
+api.connect().then(() => {
+  /* begin custom code ------------------------------------ */
+  const myAddress = 'raQZvxqUfKW3fHjxfSriAgC9Xo3sZDm9zT';
+
+  console.log('getting account info for', myAddress);
+  return api.getAccountInfo(myAddress);
+
+}).then(info => {
+  console.log(info);
+  console.log('getAccountInfo done');
+
+  /* end custom code -------------------------------------- */
+}).then(() => {
+  return api.disconnect();
+}).then(() => {
+  console.log('done and disconnected.');
+}).catch(console.error);
