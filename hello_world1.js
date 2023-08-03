5f42da4b2286a9ad7e2d5007e92008bc24464e79
Sabya <sabya7011@gmail.com>
2023-08-03 11:44:34 +0530

    added browser demo and get acc info code

---
+++ hello_world1.js
@@ -0,0 +1,25 @@
+// ********** UDEMY **************
+// Not working - except for the youtube address. 
+'use strict';
+const rippleAPI = require('ripple-lib').RippleAPI;
+
+const api = new rippleAPI({
+    server:'wss://s1.ripple.com' // Public ripple server
+});
+
+api.connect().then(() => {
+    /* begin custom code ........... */
+    const myAddress = 'rJaeYKK27YWzg5FyRQCLQbdgVyiwnbyMgL';
+    // const myAddress ='rf1BiGeXwwQoi8Z2ueFYTEXSwuJYfV2Jpn';
+
+    console.log('Getting account info for', myAddress);
+    return api.getAccountInfo(myAddress);
+}).then(info=>{
+    console.log(info);
+    console.log("Get account info is DONE");
+     /* end custom code ........... */
+}).then(()=>{
+    return api.disconnect();
+}).then(()=>{
+    console.log("Operation completed successfully and disconnected");
+}).catch(console.error);
